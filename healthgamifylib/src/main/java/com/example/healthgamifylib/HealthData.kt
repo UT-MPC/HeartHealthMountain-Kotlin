e0a02fa8ef16bfb5701b6e04ced0662ffb573476
graceleee <leegracew@gmail.com>
2021-09-22 18:36:40 -0500

    added HealthDataSource
    not yet integrated with HealthData

--- healthgamifylib/src/main/java/com/example/healthgamifylib/HealthData.kt
+++ healthgamifylib/src/main/java/com/example/healthgamifylib/HealthData.kt
@@ -2,6 +2,24 @@ package com.example.healthgamifylib
 
 import java.util.*
 
+abstract class HealthDataSource(val name: String) : Subject {
+    override var observers: MutableList<(Any?) -> Unit> = mutableListOf()
+    var value: Int = -1
+    override fun registerObserver(whatToCall: (Any?) -> Unit) {
+        observers.add(whatToCall)
+    }
+
+    override fun notifyObservers() {
+        for (o in observers) {
+            o(value)
+        }
+    }
+
+    override fun removeObserver(whatNotToCall: (Any?) -> Unit) {
+        observers.remove(whatNotToCall)
+    }
+}
+
 abstract class Data(val name: String) {
     protected var creationDate: Date = Date()  // allows backtracking
     var value: Int = -1
